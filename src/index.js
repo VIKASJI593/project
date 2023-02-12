c23d8f51483aa7cd68ce37eafb4f682b84654b53
VIKAS KUMAR <kumar.vikas593@gmail.com>
2023-02-12 11:49:24 +0530

    hello world

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-//import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// //import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+
+import React from 'react';
+import App from './App';
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
